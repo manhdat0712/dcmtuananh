f25bc81dd164e4afde59a7922b16fe1d5253cf6e
manh dat <tmdat071220@gmail.com>
2022-04-05 08:27:36 +0700

    dm tuan anh lan 2

---
+++ Bai22/Page1.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Page1.aspx.cs" Inherits="Bai22.Page1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>DANH MỤC LOẠI SÁCH</h2>
+            <asp:GridView runat="server" ID="gvBookType" Width="40%" CellPadding="4" ForeColor="Orange"></asp:GridView>
+            <asp:Button runat="server" Text="Tiếp tục" style="margin-top: 10px"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Bai22/Page1.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+
+namespace Bai22
+{
+    public partial class Page1 : System.Web.UI.Page
+    {
+        SqlDataAdapter da;
+        DataSet ds = new DataSet();
+        SqlCommand cmd = new SqlCommand();
+        String connectionString = ConfigurationManager.ConnectionStrings["conStr"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String query = "Select * from tblLoaisach";
+
+            DataSet ds = GetData(query);
+            if(ds.Tables.Count > 0)
+            {
+                gvBookType.DataSource = ds;
+                gvBookType.DataBind();
+            }
+        }
+
+        DataSet GetData(String queryString)
+        {
+            DataSet ds = new DataSet();
+
+            try
+            {
+                // Connect to the database and run the query.
+                SqlConnection connection = new SqlConnection(connectionString);
+                SqlDataAdapter adapter = new SqlDataAdapter(queryString, connection);
+
+                // Fill the DataSet.
+                adapter.Fill(ds);
+
+            }
+            catch (Exception ex)
+            {
+
+                // The connection failed. Display an error message
+            }
+
+            return ds;
+        }
+    }
+ }
+
+
+ 
